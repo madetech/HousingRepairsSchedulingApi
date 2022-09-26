8fecb4258ae77c99d3f1275d4c26ab44303ec1b1
Olly Swanson <olly.swanson95@gmail.com>
2022-09-26 11:40:07 +0100

    Read test fixture from file
    Added a helper to allow us to deserialize json test fixtures from file

--- HousingRepairsSchedulingApi.Tests/MappersTests/AppointmentsMapperTests.cs
+++ HousingRepairsSchedulingApi.Tests/MappersTests/AppointmentsMapperTests.cs
@@ -1,8 +1,6 @@
 namespace HousingRepairsSchedulingApi.Tests.MappersTests;
 
 using System;
-using System.IO;
-using System.IO.Enumeration;
 using System.Linq;
 using Dtos;
 using FluentAssertions;
@@ -12,11 +10,10 @@ using Xunit;
 
 public class AppointmentsMapperTests
 {
-    [Fact]
-    public void ShouldFilterOutAppointmentsThatAreNotBookable()
+    [Theory]
+    [JsonFileData("getAppointmentSlots.json", "twoDaysAround", typeof(GetSlotsResponse))]
+    public void ShouldFilterOutAppointmentsThatAreNotBookable(GetSlotsResponse getSlotsResponse)
     {
-        var getSlotsResponse = GetResponseFixture();
-
         var appointments = AppointmentsMapper.MapGetSlotsResponse(getSlotsResponse);
 
         appointments.Should().HaveCount(7);
@@ -46,111 +43,11 @@ public class AppointmentsMapperTests
             ]
         }
     ]}";
-        var getSlotsResponse =  JsonConvert.DeserializeObject<GetSlotsResponse>(responseJson);
+        var getSlotsResponse = JsonConvert.DeserializeObject<GetSlotsResponse>(responseJson);
 
         var appointment = AppointmentsMapper.MapGetSlotsResponse(getSlotsResponse).First();
 
         appointment.StartTime.Should().Be(DateTime.Parse("2022-09-26T08:00:00"));
         appointment.EndTime.Should().Be(DateTime.Parse("2022-09-26T17:00:00"));
     }
-
-    private static GetSlotsResponse GetResponseFixture()
-    {
-        const string responseJson = @"
-{
-    'StatusCode': '1',
-    'StatusMessage': 'SUCCESS',
-    'ContractReference': null,
-    'SlotDays': [
-        {
-            'SlotDate': '2022-09-22T00:00:00',
-            'ResourceCapacity': 0,
-            'NonBookingDay': false,
-            'Slots': []
-        },
-        {
-            'SlotDate': '2022-09-23T00:00:00',
-            'ResourceCapacity': 0,
-            'NonBookingDay': false,
-            'Slots': []
-        },
-        {
-            'SlotDate': '2022-09-26T00:00:00',
-            'ResourceCapacity': 5,
-            'NonBookingDay': false,
-            'Slots': [
-                {
-                    'Description': 'ALL',
-                    'StartTime': '08:00:00',
-                    'EndTime': '17:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 24,
-                    'MaximumSlotCapacity': 33
-                },
-                {
-                    'Description': 'SR',
-                    'StartTime': '10:00:00',
-                    'EndTime': '14:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 11,
-                    'MaximumSlotCapacity': 15
-                },
-                {
-                    'Description': 'Sat AM',
-                    'StartTime': '08:00:00',
-                    'EndTime': '12:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 11,
-                    'MaximumSlotCapacity': 15
-                },
-                {
-                    'Description': '08:00-10:00',
-                    'StartTime': '08:00:00',
-                    'EndTime': '10:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 5,
-                    'MaximumSlotCapacity': 7
-                },
-                {
-                    'Description': '10:00-12:00',
-                    'StartTime': '10:00:00',
-                    'EndTime': '12:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 5,
-                    'MaximumSlotCapacity': 7
-                },
-                {
-                    'Description': '13:00-15:00',
-                    'StartTime': '13:00:00',
-                    'EndTime': '15:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 5,
-                    'MaximumSlotCapacity': 7
-                },
-                {
-                    'Description': '15:00-17:00',
-                    'StartTime': '15:00:00',
-                    'EndTime': '17:00:00',
-                    'Bookable': true,
-                    'AvailableSlotCapacity': 5,
-                    'MaximumSlotCapacity': 7
-                }
-            ]
-        }
-    ],
-    'GasComplianceDetails': null,
-    'CalendarCentralisationDetails': {
-        'CentralisedInJobPriorityDays': true,
-        'CentralDate': '2022-09-24T14:22:10.512Z'
-    },
-    'TradeAreaDetails': {
-        'Active': false,
-        'AppointmentArea': null
-    }
-}
-";
-
-        return JsonConvert.DeserializeObject<GetSlotsResponse>(responseJson);
-    }
-
 }
