cc69b3fa2c7c842d9310771afcca1330cc0cd3b8
Olly Swanson <olly.swanson95@gmail.com>
2022-09-26 16:14:58 +0100

    Get Appointment Slots from MCM
    Although this is not a complete implementation, we have added enough
    scaffolding to the code to be able to request _some_ available
    appointments from the MCM Scheduling system.
    
    We have added Flurl as a dependency in order to have a fluent HttpClient
    with which we can also write some useful tests.
    
    A lot of the values are currently hardcoded, this is to get things
    moving, any shortcuts have been marked with "TODO" and will be addressed
    soon.

--- HousingRepairsSchedulingApi/Factories/AppointmentsFactory.cs
+++ HousingRepairsSchedulingApi/Factories/AppointmentsFactory.cs
@@ -5,22 +5,18 @@ using System.Linq;
 using Domain;
 using Dtos;
 
-public static class AppointmentsFactory
+public class AppointmentsFactory
 {
-        // remove days that are not bookable or have no slots
-        // remove slots that are not bookable
-        // combine into single list
-        public static IEnumerable<AppointmentSlot> FromGetSlotsResponse(GetSlotsResponse response) =>
-            response.SlotDays.Where(day => day.NonBookingDay == false && day.ResourceCapacity > 0).SelectMany(day =>
-            {
-                var date = day.SlotDate;
+    // remove days that are not bookable or have no slots
+    // remove slots that are not bookable
+    // combine into single list
+    public IEnumerable<AppointmentSlot> FromGetSlotsResponse(GetSlotsResponse response) =>
+        response.SlotDays.Where(day => day.NonBookingDay == false && day.ResourceCapacity > 0).SelectMany(day =>
+        {
+            var date = day.SlotDate;
 
-                return day.Slots.Select(slot =>
-                    new AppointmentSlot()
-                    {
-                        StartTime = date.Add(slot.StartTime),
-                        EndTime = date.Add(slot.EndTime),
-                    }
-                );
-            });
+            return day.Slots.Select(slot =>
+                new AppointmentSlot { StartTime = date.Add(slot.StartTime), EndTime = date.Add(slot.EndTime) }
+            );
+        });
 }
