de546e74a68b9fe8accc56511c8cb44556fd6228
jamiejordanmt <jamie.jordan@madetech.com>
2022-09-30 10:54:35 +0100

    Clean up comments and unneeded setters

--- HousingRepairsSchedulingApi/Dtos/GetSlotsRequest.cs
+++ HousingRepairsSchedulingApi/Dtos/GetSlotsRequest.cs
@@ -15,24 +15,24 @@ public class GetSlotsRequest
         this.Priority = jobCodes.PriorityCode;
         this.AppointmentDateTime = appointmentDateTime;
         this.DaysAroundReturnedDate = 14;
-        this.SlotDuration = 0;
+        this.SlotDuration = 2;
         this.CapacityWeightingOption = "CapacityOptionNormal";
         this.AddressCode = addressUprn;
         this.IncludeCalendarCentralisationDetails = true;
         this.IncludeTradeAreaDetails = true;
     }
 
-    public string ClientSystemUser { get; set; }
-    public int OperationMode { get; set; }
-    public string ContractType { get; set; }
-    public string ClientContractReference { get; set; }
-    public string TradesArray { get; set; }
-    public string Priority { get; set; }
-    public DateTime AppointmentDateTime { get; set; }
-    public int DaysAroundReturnedDate { get; set; }
-    public int SlotDuration { get; set; }
-    public string CapacityWeightingOption { get; set; }
-    public string AddressCode { get; set; }
-    public bool IncludeCalendarCentralisationDetails { get; set; }
-    public bool IncludeTradeAreaDetails { get; set; }
+    public string ClientSystemUser { get; }
+    public int OperationMode { get; }
+    public string ContractType { get; }
+    public string ClientContractReference { get; }
+    public string TradesArray { get; }
+    public string Priority { get; }
+    public DateTime AppointmentDateTime { get; }
+    public int DaysAroundReturnedDate { get; }
+    public int SlotDuration { get; }
+    public string CapacityWeightingOption { get; }
+    public string AddressCode { get; }
+    public bool IncludeCalendarCentralisationDetails { get; }
+    public bool IncludeTradeAreaDetails { get; }
 }
