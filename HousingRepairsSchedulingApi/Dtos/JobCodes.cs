867717231d7211c10a3ee08a541d0dafac01c7d4
jamiejordanmt <jamie.jordan@madetech.com>
2022-09-30 11:02:51 +0100

    Hardcode priority code

--- HousingRepairsSchedulingApi/Dtos/JobCodes.cs
+++ HousingRepairsSchedulingApi/Dtos/JobCodes.cs
@@ -4,7 +4,4 @@ public class JobCodes
 {
     public string SorCode { get; set; }
     public string TradeCode { get; set; }
-    public string PriorityCode { get; set; }
-
-    // TODO : TO ADD OTHER OTHER IDENTIFIERS DERIVED FROM SORCODE
 }
