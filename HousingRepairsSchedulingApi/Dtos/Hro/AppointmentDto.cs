11fa92f33c1c0f49105e875404e28437f8695aa9
jamiejordanmt <jamie.jordan@madetech.com>
2022-11-02 16:52:26 +0000

    Removes redundant Date prop

--- HousingRepairsSchedulingApi/Dtos/Hro/AppointmentDto.cs
+++ HousingRepairsSchedulingApi/Dtos/Hro/AppointmentDto.cs
@@ -5,7 +5,6 @@ namespace HousingRepairsSchedulingApi.Dtos.Hro;
 public record AppointmentDto
 {
     public string Id { get; init; }
-    public DateTime Date { get; init; }
     public DateTime StartTime { get; init; }
     public DateTime EndTime { get; init; }
 }
