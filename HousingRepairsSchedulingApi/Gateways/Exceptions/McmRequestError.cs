38c312076524db4aa0cd24b954ba9c96f3c3501b
Olly Swanson <olly.swanson95@gmail.com>
2022-11-10 11:12:57 +0000

    Remove linebreak from error message

--- HousingRepairsSchedulingApi/Gateways/Exceptions/McmRequestError.cs
+++ HousingRepairsSchedulingApi/Gateways/Exceptions/McmRequestError.cs
@@ -5,7 +5,7 @@ using System;
 public class McmRequestError : Exception
 {
     public McmRequestError(string errorCode, string errorMessage) : base(
-        $"StatusCode: {errorCode}\nStatusMessage: {errorMessage}")
+        $"StatusCode: {errorCode} StatusMessage: {errorMessage}")
     {
     }
 }
